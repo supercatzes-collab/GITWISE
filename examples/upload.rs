13ae8fb00807ac84f341338cab9900ef12f77c7b
uberkatze <supercatzes@gmai.com>
2026-09-20 23:24:00 +0800

    man

--- examples/upload.rs
+++ examples/upload.rs
@@ -46,15 +46,57 @@ pub fn Hero() -> Element {
 
             div { id: "upload_image",
                 h1 { id: "upload_header_image", "UPLOAD IMAGE" }
-                input { id: "upload_input_image" }
+                div {
+                    svg {
+                        width: "40",
+                        height: "40",
+                        view_box: "0 0 24 24",
+                        fill: "none",
+                        stroke: "currentColor",
+                        stroke_width: "2",
+                        stroke_linecap: "round",
+                        stroke_linejoin: "round",
+                        rect {
+                            x: "3",
+                            y: "3",
+                            width: "18",
+                            height: "18",
+                            rx: "2",
+                            ry: "2",
+                        }
+                        circle { cx: "8.5", cy: "8.5", r: "1.5" }
+                        polyline { points: "21 15 16 10 5 21" }
+                    }
+                    input { id: "upload_input_image" }
+                }
             }
 
             div { id: "upload_zipfile",
                 h1 { id: "upload_header_zipfile", "UPLOAD ZIP" }
-                input { id: "upload_input_zipfile" }
+                div {
+                    svg {
+                        width: "40",
+                        height: "40",
+                        view_box: "0 0 24 24",
+                        fill: "none",
+                        stroke: "currentColor",
+                        stroke_width: "2",
+                        stroke_linecap: "round",
+                        stroke_linejoin: "round",
+                        path { d: "M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" }
+                        polyline { points: "17 8 12 3 7 8" }
+                        line {
+                            x1: "12",
+                            y1: "3",
+                            x2: "12",
+                            y2: "15",
+                        }
+                    }
+                    input { id: "upload_input_zipfile" }
+                }
             }
 
-            button { class: "button", id: "upload_button" }
+            button { class: "button", id: "upload_button", "UPLOAD" }
         }
     }
 }
